a289361423270f528e2d8b4a4207d07d069c2d56
Aidan Miskella <aidanmiskella10@gmail.com>
2019-03-31 17:03:17 +0100

    Initial Commit

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Aidan Miskella on 31/03/2019.
+//  Copyright © 2019 Aidan Miskella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
